9f4337b166d112979e972295e1dd74cd16282e7b
“czx” <czx@zzu.com>
2022-11-05 22:06:12 +0800

    hot-fix commit2

--- src/main/java/com/czx/GitTes.java
+++ src/main/java/com/czx/GitTes.java
@@ -9,5 +9,6 @@ public class GitTes {
         System.out.println("Hello Git!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
+        System.out.println("hot-fix test!");
     }
 }
