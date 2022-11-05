f2070c2b32db8b0cdcb0bf0d0ef01b8b613591ec
“czx” <czx@zzu.com>
2022-11-05 21:57:07 +0800

    third commit

--- src/main/java/com/czx/GitTes.java
+++ src/main/java/com/czx/GitTes.java
@@ -7,5 +7,6 @@ public class GitTes {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git3!");
     }
 }
