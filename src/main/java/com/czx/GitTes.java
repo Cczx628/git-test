65360bc4bf3dd7dd6791018277caffb1e40c265f
“Cczx628” <1363953018@qq.com>
2022-11-06 00:21:13 +0800

    master commit3

--- src/main/java/com/czx/GitTes.java
+++ src/main/java/com/czx/GitTes.java
@@ -7,6 +7,7 @@ public class GitTes {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
         System.out.println("master test!");
