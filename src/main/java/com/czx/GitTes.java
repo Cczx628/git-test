edfb8c578c42f870f05ea277e1a612046ec77587
“czx” <czx@zzu.com>
2022-11-05 21:56:03 +0800

    second commit

---
+++ src/main/java/com/czx/GitTes.java
@@ -0,0 +1,11 @@
+package com.czx;
+
+/**
+ * Hello world!
+ */
+public class GitTes {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+    }
+}
